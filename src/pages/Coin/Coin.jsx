77e2913290dd1a375fc89cbb34c7d4c2b2e54215
Chetan-Salunke <chetanbusalunke@gmail.com>
2025-05-07 01:34:09 +0530

    search-links

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Coin.css'
+import { useParams } from 'react-router-dom'
+import { CoinContext } from '../../context/CoinContext';
 
 const Coin = () => {
+
+  const {coinId} = useParams();
+  const [coinData, setCoinData] = useState();
+  const {currency} = useContext(CoinContext)
+
+  const fetchCoinData = async ()=>{
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-Cg8Q91jscUzqx2mG5JquJk7E'}
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(res => res.json())
+      .then(res => setCoinData(res))
+      .catch(err => console.error(err));
+  }
+
+  useEffect(()=>{
+    fetchCoinData();
+  },[currency])
+
   return (
     <div>
-      <h1>Coin</h1>
+      <h1>Coin : {coinId}</h1>
     </div>
   )
 }
