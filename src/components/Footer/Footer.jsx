364e03c92f4d4e7aab93bfe3c56ff8f781328dcf
Chetan-Salunke <chetanbusalunke@gmail.com>
2025-05-06 23:18:31 +0530

    navbar-footer-apicall

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @ 2025, Crypto Fi - All Right Reserved. Design & Developed by <a href="https://chetansalunke.netlify.app/" className='footer-link'>Chetan Salunke</a></p>
+    </div>
+  )
+}
+
+export default Footer
